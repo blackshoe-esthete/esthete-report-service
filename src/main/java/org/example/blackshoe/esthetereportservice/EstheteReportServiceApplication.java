b174cc1d94c775c20155cddf365a2251263d3adb
Subin <70842920+bin-pro@users.noreply.github.com>
2024-06-22 08:44:47 +0900

    Build: init

---
+++ src/main/java/org/example/blackshoe/esthetereportservice/EstheteReportServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.blackshoe.esthetereportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstheteReportServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstheteReportServiceApplication.class, args);
+    }
+
+}
